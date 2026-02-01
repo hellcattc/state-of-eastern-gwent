b106d9e13a0e6d8fa85a784769bc4454ff5aa195
Nikita Zlygostev <slhtte345@gmail.com>
2026-02-01 13:55:16 +0300

    updated

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
-import { invoke } from "@tauri-apps/api";
 import { ListGuides } from "./types";
+import { invoke } from "@tauri-apps/api/tauri";
 
 async function getAllGuides () {
 	const res = await invoke<ListGuides>('get_guides')
